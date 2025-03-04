0457fb7179dcf88b59bd9d31d22ebc6211e64adb
DzhagaevSarmat <dzhagaevsarmat@gmail.com>
2025-03-04 10:35:14 +0300

    добавили модель, контроллер и представление для товара

---
+++ WebApplicationShopOnline/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationShopOnline.Models;
+
+namespace WebApplicationShopOnline.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product product = new Product("пирожок", "с ливером", 50);
+            //ViewBag.Product = product;
+            return View(product);
+        }
+
+        //public string Index()
+        //{
+        //    Product product = new Product("пирожок", "с ливером", 50);
+        //    return product.ToString(); 
+        //}
+    }
+}
